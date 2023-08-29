778eca40a5a13c5aaeea1b59b0c573239ae79e3d
Gerard Capes <gerard.capes@manchester.ac.uk>
2023-08-29 16:11:41 +0100

    Move assignments to appropriate loops
    There were multiple assignments in the inner loop that didn't
    need to be there.
    I've moved them either to inside the outer (i) loop, or even above
    that if they have no iteration dependency.

--- examples/optical_critical.m
+++ examples/optical_critical.m
@@ -34,33 +34,33 @@ function optical_critical(a, h)
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     L = 4;   % number of internal layers
     R = zeros(numnu, numtheta);
+
+    %        superstr.  |  patt. Au  |  patt. GaAs  |  unp. GaAs  | dop. GaAs | subst.
+    f     = [              .77          .77            1            1                       ]; % fraction of B in A
+    d     = [    30        .2           h              (9-h)        1                 30    ]; % microns
+    epsxA = [              1            1              1            1                       ]; % material A
+    epszA = [              1            1              1            1                       ]; %
+    sigma = [          0            0              0            0                0          ];
+
+    epssup = 1;
     for i = 1:numnu
+        % dispersive dielectric constants
+        % mind the signs of imag parts (misprint in the APL)
+        wn = nu(i)*100/3;        % wavenumber in cm^-1 !!!!!!
+        om = 2*pi*nu(i)*1e12;       % ang freq in Hz
+        epsAu = 1 - (7.25e4)^2/( wn^2 + 1i*216*wn ); % Ordal Au
+        epsGaAs = 11*(1 + (292^2 - 268^2)/(268^2 - wn^2 - 1i*2.4*wn)); % GaAs with phonon
+        epsGaAs_dop = 11 - (2.6e24*1.6e-19^2/(8.8e-12*0.067*9.1e-31*(om^2 + 1i*om/100e-15))); % doped GaAs
+        %        superstr.  |  patt. Au  |  patt. GaAs  |  unp. GaAs  | dop. GaAs | subst.
+        epsxB = [              epsAu        epsGaAs        epsGaAs      epsGaAs_dop             ]; % material B
+        epszB = [              epsAu        epsGaAs        epsGaAs      epsGaAs_dop             ]; %
+        epssub = epsAu;
+        k0   = 2*pi*nu(i)*0.01/3;         % wavevector in micron ^-1
+
         for j = 1:numtheta
             % main cycles over energy and angles
             fprintf('Frequency: %i, Theta: %i.\n', i, j)
 
-            % dispersive dielectric constants
-            % mind the signs of imag parts (misprint in the APL)
-            wn = nu(i)*100/3;        % wavenumber in cm^-1 !!!!!!
-            epsAu = 1 - (7.25e4)^2/( wn^2 + 1i*216*wn ); % Ordal Au
-            epsGaAs = 11*(1 + (292^2 - 268^2)/(268^2 - wn^2 - 1i*2.4*wn)); % GaAs with phonon
-            om = 2*pi*nu(i)*1e12;       % ang freq in Hz
-            epsGaAs_dop = 11 - (2.6e24*1.6e-19^2/(8.8e-12*0.067*9.1e-31*(om^2 + 1i*om/100e-15))); % doped GaAs
-
-            %        superstr.  |  patt. Au  |  patt. GaAs  |  unp. GaAs  | dop. GaAs | subst.
-            f     = [              .77          .77            1            1                       ]; % fraction of B in A
-            d     = [    30        .2           h              (9-h)        1                 30    ]; % microns
-
-            epsxA = [              1            1              1            1                       ]; % material A
-            epszA = [              1            1              1            1                       ]; %
-            epsxB = [              epsAu        epsGaAs        epsGaAs      epsGaAs_dop             ]; % material B
-            epszB = [              epsAu        epsGaAs        epsGaAs      epsGaAs_dop             ]; %
-            sigma = [          0            0              0            0                0          ];
-
-            epssup = 1;
-            epssub = epsAu;
-
-            k0   = 2*pi*nu(i)*0.01/3;         % wavevector in micron ^-1
             kpar = k0*sin(theta(j)*pi/180);
 
             % calling the scattering matrix subroutine for reflection
