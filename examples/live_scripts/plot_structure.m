d570c6913c2917924c7efadd0490835a0625d55c
Gerard Capes <gerard.capes@manchester.ac.uk>
2023-10-05 13:34:49 +0100

    Set colour transparancies

--- examples/live_scripts/plot_structure.m
+++ examples/live_scripts/plot_structure.m
@@ -18,7 +18,7 @@ function plot_structure(L, f, d, a)
         else
             layer_name = "Internal layer";
         end
-        yregion(start_depth(layer), end_depth(layer), FaceColor=colours(layer), DisplayName=layer_name)
+        yregion(start_depth(layer), end_depth(layer), FaceColor=colours(layer), DisplayName=layer_name, FaceAlpha=0.8)
 
         if layer_name == "Internal layer"
             internal_layer_num = internal_layer_num + 1;
