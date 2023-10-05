46e79364a507d5f19e2c4cc5518678a1b94f032d
Gerard Capes <gerard.capes@manchester.ac.uk>
2023-10-05 12:05:06 +0100

    Draw A/B mixing fraction

--- examples/live_scripts/plot_structure.m
+++ examples/live_scripts/plot_structure.m
@@ -1,7 +1,5 @@
 function plot_structure(L, f, d, a)
     % plot_structure Plot structure of material for visual check
-    % patch to fill between lines
-    % or yregion
     % L: number of layers
     % f: fraction of material B in material A
     % d: depth of layer (microns)
@@ -10,6 +8,8 @@ function plot_structure(L, f, d, a)
     end_depth = cumsum(d);
     start_depth = [0, end_depth(1:end -1)];
     colours = ["r", "b", "g", "c", "m", "y", "k"];
+    x_mid = 0.5 * a;
+    internal_layer_num = 0;
     for layer = 1:length(d)
         if layer == 1
             layer_name = "Substrate";
@@ -18,8 +18,18 @@ function plot_structure(L, f, d, a)
         else
             layer_name = "Internal layer";
         end
-
         yregion(start_depth(layer), end_depth(layer), FaceColor=colours(layer), DisplayName=layer_name)
+
+        if layer_name == "Internal layer"
+            internal_layer_num = internal_layer_num + 1;
+            fraction = f(internal_layer_num);
+
+            x1 = x_mid - 0.5 * fraction * a;
+            x2 = x_mid + 0.5 * fraction * a;
+            x = [x1, x1, x2, x2];
+            y = [start_depth(layer), end_depth(layer), end_depth(layer), start_depth(layer)];
+            patch(x, y, 'k', 'HandleVisibility', 'off', 'FaceAlpha', 0.3)
+        end
     end
     ylim([start_depth(1), end_depth(end)])
     xlim([0, a])
