00d74dc1acdf1acf490587d540a5afd5e579e7a1
Gerard Capes <gerard.capes@manchester.ac.uk>
2023-10-10 10:08:12 +0100

    Flip y (z) axis and data

--- examples/live_scripts/plot_structure.m
+++ examples/live_scripts/plot_structure.m
@@ -4,11 +4,12 @@ function plot_structure(f, d, a)
     % d: depth of layer (microns)
     % a: stripe spacing (microns)
     figure
+    set(gca, 'YDir','reverse')
     end_depth = cumsum(d);
     start_depth = [0, end_depth(1:end -1)];
     z_offset = start_depth(end);
-    start_depth = start_depth - z_offset;
-    end_depth = end_depth - z_offset;
+    start_depth = -(start_depth - z_offset);
+    end_depth = -(end_depth - z_offset);
     colours = ["b", "r", "g", "c", "m", "y", "k"];
     internal_layer_num = 0;
     for layer = 1:length(d)
@@ -33,7 +34,7 @@ function plot_structure(f, d, a)
         end
     end
     substrate_display_depth = 10;
-    ylim([(end_depth(1) - substrate_display_depth), start_depth(end) + substrate_display_depth])
+    ylim([start_depth(end) - substrate_display_depth, (end_depth(1) + substrate_display_depth)])
     xlim([-a/2, a/2])
     mu = char(181);
     ylabel("z coordinate (" + mu + "m)")
